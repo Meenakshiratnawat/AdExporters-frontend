d03b363ca139b9474cfeed4ef060b167e6f30525
Meenakshi <meenakshiratnawat0@gmail.com>
2023-04-07 12:03:01 +0530

    changed in footer & home page

--- src/core/Base.js
+++ src/core/Base.js
@@ -15,12 +15,53 @@ const Base = ({
       </div>
       <div className={className}>{children}</div>
     </div>
-    <footer className="footer   py-0">
-      <div className="container-fluid bg-success text-white text-center  py-2">
+    <footer className="footer py-0 text-white">
+      <div className="container-fluid bg-success text-white text-center py-5">
+        {/* <h5 className="text-sm">
+          If you got any questions, feel free to reach out!
+        </h5>
+        <button className="btn btn-warning btn-sm">Contact Us</button> */}
+
+        <div class="footer-sect">
+          <h4>SHIPPING &amp; RETURNS</h4>
+          <ul>
+            <li>
+              No additional shipping charges are added to any of our orders.
+            </li>
+            <li>
+              Due to Covid-19, there might be unforseen delays in the shipment
+              reaching our customers. We thank you for your patience.
+            </li>
+            <li>
+              We offer a hassle-free return policy. If you're not satisfied with
+              your purchase, you can return it within 30 days for a full refund.
+            </li>
+          </ul>
+        </div>
+
+        {/* <h4>Help</h4> */}
+        <ul className="list-unstyled pt-5">
+          <h4>Help</h4>
+          <li className="text-white !important">
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li className="text-white !important">
+            <a href="#">Shipping Policy</a>
+          </li>
+          <li className="text-white !important">
+            <a href="#">Terms of Service</a>
+          </li>
+          <li className="text-white !important">
+            <a href="#">Refund Policy</a>
+          </li>
+          <li className="text-white !important">
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
         <h5 className="text-sm">
           If you got any questions, feel free to reach out!
         </h5>
-        <button className="btn btn-warning btn-sm ">Contact Us</button>
+        <button className="btn btn-warning btn-sm">Contact Us</button>
       </div>
     </footer>
   </div>
--- src/core/Home.js
+++ src/core/Home.js
@@ -41,36 +41,74 @@ const Home = () => {
   }, []);
 
   return (
+    //     <Base title="Home Page">
+    //       <HomeCarousel />
+    //       <div className="row text-center">
+    //         <div id="collection-description" class="desktop-12 tablet-6 mobile-3">
+    //           <div class="rte">
+    //             <p data-mce-fragment="1" data-mce-style="font-size:30ppx;">
+    //               <h2>About us</h2>
+    //             </p>
+    //             <p data-mce-fragment="1">
+    //               <span data-mce-fragment="1" data-mce-style="font-weight: 600;">
+    //                 <h1>
+    //                   Since 1960 , we "Alluring Design Exporters" , is a leading
+    //                   Manufacturer , Wholesaler and Exporter of marble Sculptures ,
+    //                   Temples and Other handicraft items . We are unique mix of
+    //                   talent and meticulousness in the quest for excellence. We are
+    //                   sculpting the finest grade of marble idols with detail that
+    //                   will hold you spellbound with their mesmerising beauty ,
+    //                   representing a true reflection of craftsmanship , Indian
+    //                   values and culture.&nbsp;
+    //                 </h1>
+    //               </span>
+    //             </p>
+    //           </div>
+    //         </div>
+    //         <div class="desktop-12 tablet-6 mobile-3">
+    //           <h1 className="text-white mt-2 mb-2">Categories</h1>
+    //           <div className="row">
+    //             {categories.map((category, index) => {
+    //               return (
+    //                 <div key={index} className="col-4 mb-4">
+    //                   <CategoryCard category={category} />
+    //                 </div>
+    //               );
+    //             })}
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </Base>
+    //   );
+    // };
+
     <Base title="Home Page">
       <HomeCarousel />
-      <div className="row text-center">
-        <div id="collection-description" class="desktop-12 tablet-6 mobile-3">
-          <div class="rte">
-            <p data-mce-fragment="1" data-mce-style="font-size:30ppx;">
-              <h2>About us</h2>
-            </p>
-            <p data-mce-fragment="1">
-              <span data-mce-fragment="1" data-mce-style="font-weight: 600;">
-                <h1>
-                  Since 1960 , we "Alluring Design Exporters" , is a leading
-                  Manufacturer , Wholesaler and Exporter of marble Sculptures ,
-                  Temples and Other handicraft items . We are unique mix of
-                  talent and meticulousness in the quest for excellence. We are
-                  sculpting the finest grade of marble idols with detail that
-                  will hold you spellbound with their mesmerising beauty ,
-                  representing a true reflection of craftsmanship , Indian
-                  values and culture.&nbsp;
-                </h1>
-              </span>
-            </p>
+      <div className="row">
+        <div className="col-lg-8 mx-auto">
+          <div className="text-center mb-5">
+            <h2>About us</h2>
+          </div>
+          <div className="text-center mb-5">
+            <h1>
+              Since 1960, we "Alluring Design Exporters", is a leading
+              Manufacturer, Wholesaler and Exporter of marble Sculptures,
+              Temples and Other handicraft items. We are unique mix of talent
+              and meticulousness in the quest for excellence. We are sculpting
+              the finest grade of marble idols with detail that will hold you
+              spellbound with their mesmerising beauty, representing a true
+              reflection of craftsmanship, Indian values and culture.
+            </h1>
           </div>
         </div>
-        <div class="desktop-12 tablet-6 mobile-3">
-          <h1 className="text-white mt-2 mb-2">Categories</h1>
+      </div>
+      <div className="row">
+        <div className="col-lg-8 mx-auto">
+          <h1 className="text-center mb-5">Categories</h1>
           <div className="row">
             {categories.map((category, index) => {
               return (
-                <div key={index} className="col-4 mb-4">
+                <div key={index} className="col-lg-4 mb-4">
                   <CategoryCard category={category} />
                 </div>
               );

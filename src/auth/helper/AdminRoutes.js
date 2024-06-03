54ccabbbea6a487ef8afa0ed27df1554fb4ec309
Meenakshi Ratnawat <90496119+Meenakshiratnawat@users.noreply.github.com>
2024-06-03 22:50:28 +0530

    Update AdminRoutes.js

--- src/auth/helper/AdminRoutes.js
+++ src/auth/helper/AdminRoutes.js
@@ -17,28 +17,3 @@ function AdminRoute({ component: Component, ...rest }) {
 }
 
 export default AdminRoute;
-// import React from "react";
-// import { Route, redirect } from "react-router-dom";
-// import { isAutheticated } from "./index";
-
-// const AdminRoute = ({ component: Component, ...rest }) => {
-//   return (
-//     <Route
-//       {...rest}
-//       render={props =>
-//         isAutheticated() && isAutheticated().user.role === 1 ? (
-//           <Component {...props} />
-//         ) : (
-//           <redirect
-//             to={{
-//               pathname: "/signin",
-//               state: { from: props.location }
-//             }}
-//           />
-//         )
-//       }
-//     />
-//   );
-// };
-
-// export default AdminRoute;
